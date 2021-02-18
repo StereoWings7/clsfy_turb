d36031f01e66da8d0167d273caa3755546dbf353
Chika Kawai <chika_hrs@adagio.ocn.ne.jp>
2021-02-18 05:45:22 +0000

    initial states

---
+++ HMEq_compare.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from scipy.fft import fftshift, ifft2
+
+
+class HMEq_data:
+    head = ("head", "<i")
+    tail = ("tail", "<i")
+
+    def __init__(self, nx=512, ny=512, lx=300, ly=300) -> None:
+        pass
+        self.nx, self.ny = nx, ny
+        self.nkx, self.nky = (nx-2)//3, (ny-2)//3
+        self.lx, self.ly = lx, ly
+        self.dx, self.dy = lx/nx, ly/ny
+        self.xx = np.linspace(0, lx, nx)
+        self.yy = np.linspace(0, ly, ny)
+        self.XX, self.YY = np.meshgrid(self.xx, self.yy)
+        self.dt_pk = np.dtype(
+            [self.head, ("pk", "{0}<c16".format((2*self.nkx+1)*(self.nky+1))), self.tail])
+        self.dt_time = np.dtype([self.head, ("time", "<d"), self.tail])
+        self.pk_size = self.dt_pk.itemsize
+        self.time_size = self.dt_time.itemsize
+
+    def __del__(self):
+        pass
+
+    def datafile_phi(self, ifrun, t_spc=100):
+        ph_data = np.empty((0, (self.nx)*(self.ny)), dtype=float)
+        iter_global = 0
+        for ifile in np.arange(1, ifrun):
+            fd_pk = open("data/pk_{0:03}.bin".format(ifile), "rb")
+            fd_time = open("data/time_{0:03}.bin".format(ifile), "rb")
+            chunk_time = np.fromfile(fd_time, dtype=self.dt_time, count=10000)
+            for iter in range(chunk_time.size):
+                iter_global += 1
+                if(iter_global % t_spc == 0):
+                    fd_pk.seek(iter*self.pk_size)
+                    chunk_pk = np.fromfile(fd_pk, dtype=self.dt_pk, count=1)
+                    pk_tmp = chunk_pk[0]["pk"].reshape(
+                        (2*self.nkx+1, self.nky+1), order="F")
+                    ph_tmp = self._fft_backward(pk_tmp).flatten()
+                    ph_data = np.append(ph_data, np.array([ph_tmp]), axis=0)
+        return ph_data
+
+    def _fft_backward(self, pk):
+        pk_lx = 2*self.nkx+1
+        pk_ly = self.nky+1
+
+        pk_full = np.zeros((pk_lx, pk_ly+self.nky), dtype='c16')
+        ph = np.zeros((self.nx, self.ny), dtype='c16')
+
+        # in HMEq_FFTW, normalization coeff is applied at forward transformation
+        # (caution!! ↑that treatment is different from ordinal FFT procedure!!# )
+        # realistic condition enforcing pattern
+        coeff_norm = self.nx*self.ny
+
+        for i in range(pk_lx):
+            for j in range(pk_ly):
+                pk_full[i, j+self.nky] = coeff_norm*pk[i, j]
+        for i in range(pk_lx):
+            for j in range(1, self.nky+1):
+                ikx = i-self.nkx
+                iky = j
+                pk_full[i, j] = coeff_norm * \
+                    pk[self.nkx-ikx, self.nky-iky].conjugate()
+
+        # copy from pk_full to ph
+        for i in range(pk_lx):
+            for j in range(pk_ly+self.nky):
+                ist = self.nx//2-self.nkx
+                jst = self.ny//2-self.nky
+                ph[i+ist, j+jst] = pk_full[i, j]
+        phr = fftshift(ph)
+        return np.real(ifft2(phr))
+
+
+def gen_rand(seed=0):
+    np.random.seed(seed)
+    phi_rand = np.random.rand(nx, ny)
+    return phi_rand
+
+
+def sampling_down(phi, ndx=128, ndy=128):
+    nx = phi.shape[0]
+    ny = phi.shape[1]
+    xstep = nx//ndx
+    ystep = ny//ndy
+    phi_down = phi[::xtep, ::ystep]
+    return phi_down
---
+++ README.md
@@ -0,0 +1,4 @@
+read dates from `HMEq_FFTW` and preprocess it to be suitable for data mining.
+usage:
+1. create instance:`phi1=HMEq_data`
+2. just read it! `ph_data=phi1.datafile_phi(10,t_spc=100)`
